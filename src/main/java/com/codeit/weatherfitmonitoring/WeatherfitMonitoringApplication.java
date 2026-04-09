9c73f17c247408161f2afd8486315a3c959e9f40
OfficialHwempire <genius5375@gmail.com>
2026-04-09 14:42:10 +0900

    git initial commit

---
+++ src/main/java/com/codeit/weatherfitmonitoring/WeatherfitMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.weatherfitmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherfitMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherfitMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/weatherfitmonitoring/WeatherfitMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.weatherfitmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherfitMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
